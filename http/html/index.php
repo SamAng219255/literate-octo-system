7bb5deaa8dde58a68fb6d9aae543de62abae96c2
sam <SamAng219255@gmail.com>
2018-02-22 13:50:23 -0800

    updated

---
+++ http/html/index.php
@@ -0,0 +1,31 @@
+<?php
+	//error_reporting(0);
+	$theList = array("*");
+	
+	$filecount=0;
+	$filenames=array("*");
+
+    if ($handle = opendir('.')) {
+    while (false !== ($file = readdir($handle)))
+    {
+        if ($file != "." && $file != "index.php")
+        {
+            array_push($filenames,array($file,is_dir($file)));
+            $filecount++;
+        }
+    }
+    sort($filenames);
+	$thelist.='<ul>';
+    for ($i=0; $i<filecount; $i++) {
+		$l='file';
+		if($filenames[i][1]) {
+			$l='directory';
+		}
+		$thelist .= '<li><a href="'.$filenames[i][0].'"class = "'.$l.'" target = "_blank">'.$filenames[i][0].'</a></li>';
+	}
+    $thelist.='</ul>';
+    closedir($handle);
+    }
+?>
+
+<?php echo $thelist?>
